b7f15442e1a8cf0c88325a9015f3d57f3b566710
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-06-20 15:05:55 +0600

    ifferent way to handle events in react

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+	function handleClick() {
+		alert("button clicked");
+	}
+
+	const handleClick2 = () => {
+		alert("button 2 clicked");
+	};
+
+	const addToFive = (num) => {
+		alert(num + 5);
+	};
+
+	return (
+		<>
+			<h3>React Core Concepts 2</h3>
+			{/* <button onclick="handleClick()">Click Me</button> */}
+			<button onClick={handleClick}>Click Me</button>
+			<button onClick={handleClick2}>Click2</button>
+			<button
+				onClick={() => {
+					alert("third clicked");
+				}}
+			>
+				third
+			</button>
+            {/* vejailla */}
+			<button onClick={() => addToFive(3)}>Four</button>
+		</>
+	);
+}
+
+export default App;
